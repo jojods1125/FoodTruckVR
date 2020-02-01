ae8aa64d16e81cad5c1f7179882a17c39a22895e
Nico <nico@hbcafe.com>
2020-02-01 15:15:57 -0500

    Completed basic ingriedient drop

--- FoodTruckVR/Assets/Scripts/Scoop.cs
+++ FoodTruckVR/Assets/Scripts/Scoop.cs
@@ -7,15 +7,22 @@ public class Scoop : MonoBehaviour
 {
     public GameObject child;
     // Update is called once per frame
-    public 
-    void Update()
-    {
-        if (child.transform.position.y < gameObject.transform.position.y)
-        {
-            Debug.Log("Drop stuff");
-        }
+    public IngredientType Scoopee;
 
-        Debug.Log("Don't");
+    public GameObject ingredSpawn1;
+    public Ingredient IngredTemp;
+    public void Update()
+    {
+        if (child.transform.position.y < gameObject.transform.position.y && Scoopee != IngredientType.None)
+        {
+            Ingredient dropped;
+            dropped = Instantiate(IngredTemp, ingredSpawn1.transform) as Ingredient;
+            dropped.IngredientType = Scoopee;
+            Scoopee = IngredientType.None;
+            
+            Debug.Log("Dropped");
+            //Destroy(ingredSpawn1, .5f);
+        }
     }
 
 }
