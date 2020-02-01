b5e07123ecfc27482cf7b9df4ef25f06f7ca182b
Ben <bfjohns4@ncsu.edu>
2020-02-01 02:17:27 -0500

    Created food and ingredient base files
    3rd time's a charm

---
+++ FoodTruckVR/Assets/Scripts/Food/FoodContainer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/** All possible food containers in the game */
+public enum FoodContainerType
+{
+	Taco,
+	Bowl
+}
+
+/** Attached to a food container in game so it can track what ingredients it currently holds */
+public class FoodContainer : MonoBehaviour
+{
+	/// <summary>
+	/// List of "sockets" for attaching an ingredient model to the container.
+	/// Starts at front of list and continues (socket at index 0 used first)
+	/// </summary>
+	public List<Transform> Sockets;
+
+	/// <summary>
+	/// All ingredients currently attached to this container
+	/// </summary>
+	public List<Ingredient> Ingredients;
+}
---
+++ FoodTruckVR/Assets/Scripts/Food/Ingredient.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/** All the different types of ingredients that can appear in the game */
+public enum IngredientType
+{
+	Beef,
+	Cheese,
+	Lettuce,
+	Tomato,
+	Bean,
+}
+
+/** Attached to in-game objects to identify what ingredient they are */
+public class Ingredient : MonoBehaviour
+{
+	/** This ingredient's type */
+	public IngredientType IngredientType;
+}
