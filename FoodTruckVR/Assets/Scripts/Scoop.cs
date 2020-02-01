9f67a02ea3f3d92ba67c4c5d152588e3efab2cb6
Nico <nico@hbcafe.com>
2020-02-01 13:32:58 -0500

    Created scoop class and scoop drop check

---
+++ FoodTruckVR/Assets/Scripts/Scoop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Scoop : MonoBehaviour
+{
+    public GameObject child;
+    // Update is called once per frame
+    void Update()
+    {
+        if (child.transform.position.y < gameObject.transform.position.y)
+        {
+            Debug.Log("Drop stuff");
+
+        }
+    }
+
+}
