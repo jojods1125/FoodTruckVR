44fec311b7e8f142502e544df2e6d4e80c7098e3
Ben Johnson <bfjdevmail@gmail.com>
2020-02-01 16:01:40 -0500

    Created the breakable monobehavior

---
+++ FoodTruckVR/Assets/Scripts/Breakables/Breakable.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** All the different ways things can break in the game */
+public enum BreakType
+{
+    BreakOff,
+}
+
+/** Attach to objects in game that have components that can break in some way */
+public class Breakable : MonoBehaviour
+{
+    /** Maps break points on the object and their chance to break */
+    public struct BreakPoint
+    {
+        /** Auto-generated ID */
+        [HideInInspector]
+        public int ID;
+
+        [Tooltip("The breakpoint transform on the object")] 
+        public Transform Socket;
+
+        [Tooltip("The break type that occurs at this spot")] 
+        public BreakType BreakType;
+
+        [Tooltip("The chance for the thing at this breakpoint to break/explode (higher weight = more likely)")]
+        public int Weight;
+    }
+
+    [Tooltip("Name of this breakable object")]
+    public string Name;
+
+    [Tooltip("List of all breakpoints on the object, paired with their probability to break")]
+    public List<BreakPoint> BreakPoints;
+
+    [Tooltip("The chance for something on this thing to break/explode (higher weight = more likely)")]
+    public int Weight;
+
+    /** Total weight of the breakpoints attached to this object */
+    private int _TotalWeight;
+
+    /** Initialize the ID's of the breakpoints and add up weights */
+    public void Initialize()
+    {
+        for( int i = 0; i < BreakPoints.Count; i++)
+        {
+            BreakPoint bp = BreakPoints[i];
+            bp.ID = i;
+            _TotalWeight += bp.Weight;
+        }
+    }
+
+    /** Choose something at random to break on this object */
+    public bool BreakSomethingAtRandom()
+    {
+        int randomWeight = Random.Range(0, _TotalWeight) + 1;
+
+        foreach(BreakPoint breakPoint in BreakPoints)
+        {
+            randomWeight -= breakPoint.Weight;
+
+            if(randomWeight <= 0)
+            {
+                Transform socket = breakPoint.Socket;
+
+                switch(breakPoint.BreakType)
+                {
+                    case BreakType.BreakOff:
+                        //detach child if child exists
+                        if(socket.childCount > 0)
+                        {
+                            socket.DetachChildren();
+                            return true;
+                        }
+                        break;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /** Cause a breakpoint at the transform with matching ID */
+    public bool Break(int inID)
+    {
+        foreach(BreakPoint breakPoint in BreakPoints)
+        {
+            if(breakPoint.ID == inID)
+            {
+                Transform socket = breakPoint.Socket;
+
+                switch(breakPoint.BreakType)
+                {
+                    case BreakType.BreakOff:
+                        //detach child if child exists
+                        if(socket.childCount > 0)
+                        {
+                            socket.DetachChildren();
+                            return true;
+                        }
+                        break;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+
+
+}
---
+++ FoodTruckVR/Assets/Scripts/Breakables/BreakableManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableManager : MonoBehaviour
+{
+    [Tooltip("List of all breakable objects in the world. IF ITS NOT HERE ITS NOT GONNA BREAK")]
+    [SerializeField]
+    List<Breakable> _AllBreakables;
+
+    /** Total weight of all breakables in scene */
+    private int _TotalWeight;
+
+    /** Initialize Systems */
+    void Awake()
+    {
+        foreach(Breakable breakable in _AllBreakables)
+        {
+            breakable.Initialize();
+            _TotalWeight += breakable.Weight;
+        }
+    }
+
+    /** Choose a breakable to break at random */
+    void BreakRandomObject()
+    {
+        int randomIndex = Random.Range(0, _AllBreakables.Count);
+
+        _AllBreakables[randomIndex].BreakSomethingAtRandom();
+    }
+
+    /** Choose an object to break at random, considering the weights of the objects*/
+    void BreakRandomObjectWeighted()
+    {
+        int randomWeight = Random.Range(0, _TotalWeight) + 1;
+
+        foreach(Breakable breakable in _AllBreakables)
+        {
+            randomWeight -= breakable.Weight;
+
+            if(randomWeight <= 0)
+            {
+                breakable.BreakSomethingAtRandom();
+                return;
+            }
+        }
+    }
+
+    /** Break something at random on the object with the given name */
+    void BreakObjectByNameRandomly(string inName)
+    {
+        foreach(Breakable breakable in _AllBreakables)
+        {
+            if(breakable.Name.Equals(inName) == true)
+            {
+                breakable.BreakSomethingAtRandom();
+            }
+        }
+    }
+
+    /** Break an object with matching name at the specific socket */
+    void BreakSpecificPointOnObject(string inName, int inSocketID)
+    {
+        foreach(Breakable breakable in _AllBreakables)
+        {
+            if(breakable.Name.Equals(inName) == true)
+            {
+                breakable.Break(inSocketID);
+            }
+        }
+    }
+}
