9541f4501b0ef7fe3d224e9987fdcf4a761e9e1a
Joseph Dasilva <jojods1125@gmail.com>
2020-02-01 16:23:16 -0500

    TACO FUNCTIONALITY IS ALMOST COMPLETE. PENDING CONTAINER UPDATE.

---
+++ FoodTruckVR/Assets/Scripts/Food/PhysicalIngredient.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalIngredient : Ingredient
+{
+    public bool CanGrab = true;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<FoodContainer>() != null)
+        {
+            collision.gameObject.GetComponent<FoodContainer>().AddIngredient(this);
+        } else
+        {
+            Debug.Log("Failure");
+        }
+    }
+
+}
