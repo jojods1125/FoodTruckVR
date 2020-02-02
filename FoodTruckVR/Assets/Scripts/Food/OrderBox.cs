8f2b34d060e4fa4d20960d8fec31a05752e09620
Blaine Carrick <blainecarrick2@gmail.com>
2020-02-01 22:00:38 -0500

    Started Orders

---
+++ FoodTruckVR/Assets/Scripts/Food/OrderBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderBox : MonoBehaviour
+{
+    public void NewOrder()
+    {
+        int tempRand = Random.Range(1, 10);
+        Debug.Log(tempRand.ToString());
+        //if(tempRand)
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            NewOrder();
+        }
+    }
+}
