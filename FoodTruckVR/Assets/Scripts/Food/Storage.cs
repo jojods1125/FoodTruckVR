7b174ad652c2ae010f484474c0ad303a2e93ba32
Joseph Dasilva <jojods1125@gmail.com>
2020-02-01 21:52:20 -0500

    plz

--- FoodTruckVR/Assets/Scripts/Food/Storage.cs
+++ FoodTruckVR/Assets/Scripts/Food/Storage.cs
@@ -14,11 +14,6 @@ public class Storage : MonoBehaviour
 
     public Refill RefillTempPref;
 
-    public void StorageClose()
-    {
-        
-    }
-
     void OnTriggerEnter(Collider refillCollide)
     {
 
@@ -61,15 +56,11 @@ public class Storage : MonoBehaviour
     {
             Refill tempRefillInst;
 
-            Debug.Log("make something");
-            Debug.Log(hasChe.ToString() + hasLet.ToString() + hasTom.ToString());
-
             if (hasChe <= 0)
             {
                 tempRefillInst = Instantiate(RefillTempPref);
                 tempRefillInst.IngredientType = IngredientType.Cheese;
                 tempRefillInst.transform.position = cheSpawn.transform.position;
-                Debug.Log("make cheese");
             }
             if (hasLet <= 0)
             {
@@ -87,10 +78,9 @@ public class Storage : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown("space"))
-        {
-            Debug.Log("pressed space");
+       // if (Input.GetKeyDown("space"))
+        //{
             StorageFill();
-        }
+        //}
     }
 }
