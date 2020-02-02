fa3edad97c7f4f918882c1e8d6dcf460de11a7eb
Mitchell Dunning <ankhman12@gmail.com>
2020-02-02 01:59:19 -0500

    CO2 coming at ya (Fire Extinguisher Made)

---
+++ FoodTruckVR/Assets/Scripts/Food/ExtinguisherParticles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtinguisherParticles : MonoBehaviour
+{
+    public ParticleSystem Extinguisher;
+    public Rigidbody FireExt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Extinguisher = GetComponent<ParticleSystem>();
+        FireExt = GetComponent<Rigidbody>();
+        Extinguisher.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FireExt.GetComponent<OVRGrabbable>().isGrabbed) {
+            Extinguisher.Play();
+        }
+    }
+}
